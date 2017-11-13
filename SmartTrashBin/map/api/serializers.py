bb8d632324e1a13c7e2ce42b0c903a6591cb6f77
T.C.Havy <thomashavy@hotmail.fr>
2017-11-13 23:59:51 +0100

    Created API folder and moved files

---
+++ SmartTrashBin/map/api/serializers.py
@@ -0,0 +1,19 @@
+""" Contains serializers for the models
+
+A serializer is a tool that converts data from the database into a readable or
+transmittable format (JSON in this case).
+"""
+
+from rest_framework import serializers
+from .models import TrashBin, TrashBinState
+
+class TrashBinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrashBin
+        #fields = ('pk', 'latitude', 'longitude', 'floor', 'building', 'state')
+        #read_only_fields = ('date_created', 'date_modified')
+
+class TrashBinStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrashBinState
+        fields = ('fullness', 'time')
