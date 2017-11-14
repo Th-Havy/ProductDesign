25b47441682b05e59a028c3430b2691112ac22f3
T.C.Havy <thomashavy@hotmail.fr>
2017-11-14 10:21:40 +0100

    Modified views.py in api folder

--- SmartTrashBin/map/api/views.py
+++ SmartTrashBin/map/api/views.py
@@ -1,2 +1,22 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from map.models import TrashBin, TrashBinState
+from .serializers import TrashBinSerializer, TrashBinStateSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+
+class TrashBinList(ListCreateAPIView):
+    queryset = TrashBin.objects.all()
+    serializer_class = TrashBinSerializer
+
+
+class TrashBinListDetail(RetrieveUpdateDestroyAPIView):
+    queryset = TrashBin.objects.all()
+    serializer_class = TrashBinSerializer
+
+class TrashBinStateList(ListCreateAPIView):
+    queryset = TrashBinState.objects.all()
+    serializer_class = TrashBinStateSerializer
+
+
+class TrashBinStateListDetail(RetrieveUpdateDestroyAPIView):
+    queryset = TrashBinState.objects.all()
+    serializer_class = TrashBinStateSerializer
