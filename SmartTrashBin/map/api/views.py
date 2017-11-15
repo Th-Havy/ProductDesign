3e1c3f5f2b18d8306b80c4717e900479fe8d397e
T.C.Havy <thomashavy@hotmail.fr>
2017-11-15 01:54:26 +0100

    Corrected updating of states

--- SmartTrashBin/map/api/views.py
+++ SmartTrashBin/map/api/views.py
@@ -18,9 +18,21 @@ class TrashBinStateList(ListCreateAPIView):
     serializer_class = TrashBinStateSerializer
 
     def get_queryset(self):
-        """ Return the states of a particular trash bin"""
+        """Return the states of a particular trash bin"""
         trashId = self.kwargs['pk']
         return TrashBinState.objects.filter(trashBin__pk=trashId)
+
+    def perform_create(self, serializer):
+        """Called when a new state is added"""
+        # Make sure to add the state to the good trash bin
+        trashBin=TrashBin.objects.get(pk=self.kwargs['pk'])
+        state = serializer.save(trashBin=trashBin)
+
+        # If the state time is more recent, update the trash bin state
+        if trashBin.state.time <= state.time:
+            trashBin.state = state
+            trashBin.save()
+
 
 # Is this one necessary ? Maybe RetrieveAPIView is enough ?
 #class TrashBinStateDetail(RetrieveUpdateDestroyAPIView):
