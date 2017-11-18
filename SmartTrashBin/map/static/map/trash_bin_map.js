ef5a27dce1778f63ef8329375b5e7ebba347458e
T.C.Havy <thomashavy@hotmail.fr>
2017-11-18 17:33:54 +0100

    Test of google map

--- SmartTrashBin/map/static/map/trash_bin_map.js
+++ SmartTrashBin/map/static/map/trash_bin_map.js
@@ -1,20 +1,49 @@
+var api_endpoint = "http://127.0.0.1:8000/api/trashbin/"
+
 function initMap() {
 var EPFL_CENTER_LATITUDE = 46.518349;
 var EPFL_CENTER_LONGITUDE = 6.568052;
 var DEFAULT_ZOOM = 18;
 
-var mapProp= {
+// To hide google maps points of interest
+var mapStyles =[
+    {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [
+              { visibility: "off" }
+        ]
+    }
+];
+
+var mapProperties= {
     center:new google.maps.LatLng(EPFL_CENTER_LATITUDE, EPFL_CENTER_LONGITUDE),
     zoom:DEFAULT_ZOOM,
+    styles:mapStyles,
+    // Remove street view button
+    streetViewControl:false,
+    // Hide satellite view button
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP]
+    },
 };
-var map=new google.maps.Map(document.getElementById("trashBinMap"),mapProp);
 
-var rolex = {lat: 46.518349, lng: 6.568052};
+// Create the map on the webpage
+var map=new google.maps.Map(document.getElementById("trashBinMap"),mapProperties);
+
+// Fetch the position of the trash bins
+$(document).ready(function() {
+    $.ajax({
+        url: api_endpoint
+    }).then(function(data) {
+        console.log(data[0].building);
+    });
+});
+
+/*var rolex = {lat: EPFL_CENTER_LATITUDE, lng: EPFL_CENTER_LONGITUDE};
 
 var marker1 = new google.maps.Marker({
           position: rolex,
           map: map
-        });
-
-console.log("Ta mère")
+      });*/
 }
