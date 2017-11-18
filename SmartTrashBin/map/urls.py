758ad1706acc0453444039c4d51ccefc46af5d1c
T.C.Havy <thomashavy@hotmail.fr>
2017-11-18 16:46:28 +0100

    Test of google map

--- SmartTrashBin/map/urls.py
+++ SmartTrashBin/map/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="trash_bin_map.html")),
+    url(r'^$', TemplateView.as_view(template_name="map/trash_bin_map.html")),
 ]
