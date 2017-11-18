3a2bc53b3f00fec74b908a77e674453c317b422a
T.C.Havy <thomashavy@hotmail.fr>
2017-11-18 16:19:23 +0100

    Added trash_bin_map.js

---
+++ SmartTrashBin/map/static/trash_bin_map.js
@@ -0,0 +1,19 @@
+function myMap()
+{
+var EPFL_CENTER_LATITUDE = 46.518349;
+var EPFL_CENTER_LONGITUDE = 6.568052;
+var DEFAULT_ZOOM = 18;
+
+var mapProp= {
+    center:new google.maps.LatLng(EPFL_CENTER_LATITUDE, EPFL_CENTER_LONGITUDE),
+    zoom:DEFAULT_ZOOM,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var rolex = {lat: 46.518349, lng: 6.568052};
+
+var marker1 = new google.maps.Marker({
+          position: rolex,
+          map: map
+        });
+}
