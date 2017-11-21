144e91c744af1651f83febce032b4ee83c393ee0
T.C.Havy <thomashavy@hotmail.fr>
2017-11-21 12:45:39 +0100

    Added clustering by building

--- SmartTrashBin/map/static/map/trash_bin_map.js
+++ SmartTrashBin/map/static/map/trash_bin_map.js
@@ -5,6 +5,9 @@ var EPFL_CENTER_LATITUDE = 46.518349;
 var EPFL_CENTER_LONGITUDE = 6.568052;
 var DEFAULT_ZOOM = 18;
 
+// Variable containing the trash bin positions grouped by building
+var buildingClusters = {};
+
 // To hide google maps points of interest
 var mapStyles =[
     {
@@ -32,16 +35,40 @@ var mapProperties= {
 var map=new google.maps.Map(document.getElementById("trashBinMap"),mapProperties);
 
 // Fetch the position of the trash bins and display markers
-$(document).ready(function() {
-    $.ajax({
+$(document).ready(function()
+{
+    $.ajax(
+    {
         url: api_endpoint
-    }).then(function(data) {
-        data.forEach(function(trashbin) {
-            new google.maps.Marker({
+    }).then(function(data)
+    {
+        data.forEach(function(trashbin)
+        {
+            // Create a marker for each trashbin
+            var marker = new google.maps.Marker(
+            {
                 position: {lat: trashbin.latitude, lng: trashbin.longitude},
-                map: map
+                map: map,
+                label: String(trashbin.state.fullness)
             });
+
+            if (buildingClusters[trashbin.building] == undefined)
+            {
+                buildingClusters[trashbin.building] = [];
+            }
+
+            buildingClusters[trashbin.building].push(marker);
         });
+
+        // Group trash bins by building
+        for(var building in buildingClusters)
+        {
+           var markerCluster = new MarkerClusterer(map, buildingClusters[building],
+               {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+                averageCenter: true,
+                gridSize: 100});
+        }
+
     });
 });
 
